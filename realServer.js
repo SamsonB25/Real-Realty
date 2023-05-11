d3441ee1cedaa65a859b811053bf1d9869ad74ed
SamsonB25 <SamsonB25@yahoo.com>
2023-05-10 22:56:19 -0500

    moved db info back into db.js

--- realServer.js
+++ realServer.js
@@ -1,15 +1,4 @@
 import app from "./middleware.js";
-import pkg from "pg";
-const { Pool } = pkg;
-import dotenv from "dotenv";
-
-dotenv.config();
-
-console.log(process.env.DATABASE_URL);
-
-export const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
 
 const PORT = 3000;
 
