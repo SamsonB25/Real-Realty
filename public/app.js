1c36f9d42738fc41e156fe78c2a5b6236e9195bd
SamsonB25 <SamsonB25@yahoo.com>
2023-05-11 22:46:10 -0500

    Realtors get request for data

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ async function getproperties() {
     const response = await axios.get(
       "https://real-realty.onrender.com/properties"
     );
-    console.log(response);
+    console.log(response.data);
   } catch (error) {}
 }
 getproperties();
@@ -15,7 +15,7 @@ async function getRealtors() {
     const response = await axios.get(
       "https://real-realty.onrender.com/realtors"
     );
-    console.log(response);
+    console.log(response.data);
   } catch (error) {}
 }
 getRealtors();
